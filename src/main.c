cd0f6114eac0c544bc6da763dbf40972e01152d4
Maciej Kasprzyk <maciejkasprzyk98@gmail.com>
2020-01-18 14:16:22 +0100

    parsujemy stringa zamiast stdinina

--- src/main.c
+++ src/main.c
@@ -6,10 +6,26 @@
 
 extern int yyparse();
 
-int main(int argc, char **argv, char *envp[]) {
-    printf("Wypisano z maina\n");
-    initShell();
-    yyparse();
+typedef struct yy_buffer_state *YY_BUFFER_STATE;
+extern YY_BUFFER_STATE yy_scan_string(const char *str);
+extern void yy_delete_buffer(YY_BUFFER_STATE buffer);
 
-    return 0;
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
+int main(int argc, char **argv, char *envp[]) {
+    initShell();
+
+    while(1) {
+        printf("> ");
+        char *line = NULL;
+        size_t n;
+        getline(&line, &n, stdin);
+        YY_BUFFER_STATE buffer = yy_scan_string(line);
+
+        yyparse();
+        yy_delete_buffer(buffer);
+        free(line);
+    }
 }
+#pragma clang diagnostic pop
